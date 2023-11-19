a7b5552ba1e1de820035d6ddc71b66de505222d1
Khaled Jallouli <jallouli.khaled.2001@gmail.com>
2023-11-19 20:36:52 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -26,3 +26,16 @@ class BaseModel:
 
         self.updated_at = datetime.today()
         models.storage.save()
+        
+    def to_dict(self):
+
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
+
+    def __str__(self):
+
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
