eb85709aeb4b7ec6acad72fa41062f1fd9e5253f
Rayen-JN <rayenchuk@gmail.com>
2023-11-19 20:32:19 +0100

    base model

---
+++ models/base_model.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import models
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+
+    def __init__(self, *args, **kwargs):
+
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
+
+    def save(self):
+
+        self.updated_at = datetime.today()
+        models.storage.save()
