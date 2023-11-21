b31c144cd3d78f7ed7dca4b3dc7e069660b0e670
Rayen-JN <rayenchuk@gmail.com>
2023-11-21 19:35:01 +0100

    Update File add Comment

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -4,10 +4,13 @@ from models.amenity import Amenity
 
 class TestAmenity(unittest.TestCase):
     def test_amenity_name_default_value(self):
+        """Test that the default value of the amenity name
+        is an empty string."""
         amenity = Amenity()
         self.assertEqual(amenity.name, "")
 
     def test_amenity_name_assignment(self):
+        """Test that the amenity name can be assigned a new value."""
         amenity = Amenity()
         amenity.name = "Swimming Pool"
         self.assertEqual(amenity.name, "Swimming Pool")
