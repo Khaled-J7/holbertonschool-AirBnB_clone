cb0326e103be96517f7c44353e454a6c3a5a3bfd
Khaled Jallouli <jallouli.khaled.2001@gmail.com>
2023-11-21 19:57:43 +0100

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -25,3 +25,41 @@ class TestHBNBCommandMethods(unittest.TestCase):
     def test_quit_command(self):
         """Test quit command"""
         self.assertTrue(self.console.do_quit(""))
+
+    def test_EOF_command(self):
+        """Test EOF command"""
+        self.assertTrue(self.console.do_EOF(""))
+
+    def test_emptyline(self):
+        """Test emptyline method"""
+        self.assertIsNone(self.console.emptyline())
+
+    def test_help_quit(self):
+        """Test help for quit command"""
+        captured_output = self.capture_output(self.console.help_quit)
+        self.assertEqual(captured_output, "Quit command to exit the program")
+
+    def test_create_missing_class_name(self):
+        """Test create command with missing class name"""
+        captured_output = self.capture_output(self.console.do_create)
+        self.assertEqual(captured_output, "** class name missing **")
+
+    def test_create_invalid_class_name(self):
+        """Test create command with invalid class name"""
+        captured_output = self.capture_output(
+            self.console.do_create, "InvalidClassName")
+        self.assertEqual(captured_output, "** class doesn't exist **")
+
+    def test_create_valid_class(self):
+        """Test create command with valid class name"""
+        captured_output = self.capture_output(self.console.do_create, "User")
+        self.assertTrue(captured_output)  # Ensure that an ID is generated
+
+    def test_show_missing_class_name(self):
+        """Test show command with missing class name"""
+        captured_output = self.capture_output(self.console.do_show, "")
+        self.assertEqual(captured_output, "** class name missing **")
+
+
+if __name__ == '__main__':
+    unittest.main()
