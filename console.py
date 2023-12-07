e870f2fe8b6539c0de44265338c45cf30d983f04
Khaled Jallouli <jallouli.khaled.2001@gmail.com>
2023-12-07 10:56:33 +0100

    Update console.py

--- console.py
+++ console.py
@@ -9,7 +9,7 @@ from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
-from models import FileStorage
+from storage import FileStorage
 
 
 class HBNBCommand(cmd.Cmd):
