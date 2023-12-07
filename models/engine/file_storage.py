09dff634ec292ef55f737276093143d15c9199a5
Khaled Jallouli <jallouli.khaled.2001@gmail.com>
2023-12-07 12:07:51 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Defines the fileStorage class"""
+
+from models import storage
 from models.base_model import BaseModel
 from models.user import User
 from models.amenity import Amenity
