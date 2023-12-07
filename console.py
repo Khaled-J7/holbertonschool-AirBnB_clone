43e6ec7529d03cac77a7a6324b8787b327dde1af
Khaled Jallouli <jallouli.khaled.2001@gmail.com>
2023-12-07 12:06:20 +0100

    Update console.py again

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """console module"""
 
+from models import storage
 import cmd
 from models.base_model import BaseModel
 from models.user import User
@@ -10,7 +11,7 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.engine.file_storage import FileStorage
-from models import storage
+
 
 
 class HBNBCommand(cmd.Cmd):
